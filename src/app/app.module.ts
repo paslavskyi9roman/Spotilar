fd78612340d52132406e9d1e0c329ed1974df01b
paslavskyi9roman <paslavsky.roman@gmail.com>
2021-02-22 14:46:59 +0200

    Styling the Header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { GenreComponent } from './pages/genre/genre.component';
-import { LikedComponent } from './pages/liked/liked.component';
 import { GenresComponent } from './components/genres/genres.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NoPageFoundComponent } from './no-page-found/no-page-found.component';
+import { LikedComponent } from './components/liked/liked.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     GenreComponent,
-    LikedComponent,
     GenresComponent,
+    LikedComponent,
     HomeComponent,
     NoPageFoundComponent
   ],
