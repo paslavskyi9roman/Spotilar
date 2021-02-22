c4d92d32ceee2f517207a8e471f3473f1ed3fe27
paslavskyi9roman <paslavsky.roman@gmail.com>
2021-02-22 11:40:58 +0200

    styling the Genres component

--- src/app/components/genres/genres.component.ts
+++ src/app/components/genres/genres.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./genres.component.scss']
 })
 export class GenresComponent implements OnInit {
-  genres = ['rock', 'meatal', 'punk', 'rap', 'pop', 'EDM']
+  genres = ['rock', 'metal', 'punk', 'rap', 'pop', 'EDM']
 
   constructor() { }
 
