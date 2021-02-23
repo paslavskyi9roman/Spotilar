c274e3bb5398d1737dc9b99861255063a4daf08b
paslavskyi9roman <paslavsky.roman@gmail.com>
2021-02-23 13:34:50 +0200

    receiving a album covre url

--- src/app/pages/genre/genre.component.ts
+++ src/app/pages/genre/genre.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { DataService } from '../../services/data.service';
-
 @Component({
   selector: 'app-genre',
   templateUrl: './genre.component.html',
@@ -11,6 +10,7 @@ export class GenreComponent implements OnInit {
   selectedGenre = this.actRoute.snapshot.params.id;
 
   albums: any;
+
   
   constructor(private actRoute: ActivatedRoute, private dataService: DataService) {
     console.log('selected genre is ' + this.selectedGenre)
@@ -19,7 +19,6 @@ export class GenreComponent implements OnInit {
   ngOnInit(): void {
     this.dataService.getAlbums(this.selectedGenre).subscribe((albums) => {
       this.albums = albums['albums']['album'];
-      console.log(`list of all albums ${albums['albums']['album']}`)
     })
   }
  
